68c4a6835c26e3dbe6e960c10a5df9073a7bddb4
Ala-Mabrouk <58920704+Ala-Mabrouk@users.noreply.github.com>
2022-06-22 07:57:39 +0100

    media upload to server

--- BackEnd_nodeJs/index.js
+++ BackEnd_nodeJs/index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 
 const userRoute = require("./Routes/userRoutes")
 const projectRoute = require("./Routes/projectRoutes.js")
+const multer = require('multer');
 
 
 const app = express()
@@ -13,6 +14,56 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(cors())
+
+// Configure Storage
+var storage = multer.diskStorage({
+
+    // Setting directory on disk to save uploaded files
+    destination: function (req, file, cb) {
+        cb(null, './Uploads')
+    },
+
+    // Setting name of file saved
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+var upload = multer({
+    storage: storage,
+    limits: {
+        // Setting Image Size Limit to 2MBs
+        fileSize: 2000000
+    },
+    // fileFilter(req, file, cb) {
+    //     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+    //         //Error 
+    //         cb(new Error('Please upload JPG and PNG images only!'))
+    //     }
+    //     //Success 
+    //     cb(undefined, true)
+    // }
+})
+
+app.post("/files/upload", upload.single("uploadedImage"), (req, res, next) => {
+    const file = req.file
+    console.log(req);
+    if (!file) {
+        const error = new Error('Please upload a file')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+    res.status(200).send({
+        statusCode: 200,
+        status: 'success',
+        uploadedFile: file
+    })
+
+}, (error, req, res, next) => {
+    res.status(400).send({
+        error: error.message
+    })
+}
+)
 
 app.use('/user', userRoute)
 app.use('/project', projectRoute)
--- front-end/src/app/project-details/project-details.component.ts
+++ front-end/src/app/project-details/project-details.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Project } from '../Models/project';
 import { ProjectService } from '../Services/project.service';
@@ -13,14 +14,22 @@ export class ProjectDetailsComponent implements OnInit {
 
   uploadedFiles: Array<File> = [];
 
+  @ViewChild('UploadFileInput', { static: false }) uploadFileInput: any;
+  fileUploadForm: any;
+  fileInputLabel: any;
+
   constructor(
     private projectService: ProjectService,
     private route: ActivatedRoute,
-    private http: HttpClient
+    private http: HttpClient,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
     this.initData();
+    this.fileUploadForm = this.formBuilder.group({
+      uploadedImage: [''],
+    });
   }
   initData() {
     let idP = this.route.snapshot.params['Pid'];
@@ -29,30 +38,53 @@ export class ProjectDetailsComponent implements OnInit {
       this.projectValue = Project.fillProjectFromJSON(res[0]);
     });
   }
-  fileChange(element: any) {
-    this.uploadedFiles = element.target.files;
+
+  onFileSelect(event: any) {
+    const file = event.target.files[0];
+    this.fileInputLabel = file.name;
+    this.fileUploadForm.get('uploadedImage').setValue(file);
   }
-  upload() {
-    let formData = new FormData();
-    for (var i = 0; i < this.uploadedFiles.length; i++) {
-      const extension = this.uploadedFiles[i].name.substring(
-        this.uploadedFiles[i].name.lastIndexOf('.'),
-        this.uploadedFiles[i].name.length
-      ); //chnaging file name to smthing identified :
-      // projectName_dateAdded.ext
-      const newName =
-        this.projectValue.projectFullName +
-        '_' +
-        new Date().toISOString() +
-        extension;
 
-      formData.append('uploads[]', this.uploadedFiles[i], newName);
+  onFormSubmit() {
+    if (!this.fileUploadForm.get('uploadedImage').value) {
+      alert('Please fill valid details!');
+      return false;
     }
+    const formData = new FormData();
+    const extension = this.fileUploadForm
+      .get('uploadedImage')
+      .value.name.substring(
+        this.fileUploadForm.get('uploadedImage').value.name.lastIndexOf('.'),
+        this.fileUploadForm.get('uploadedImage').value.name.length
+      ); //chnaging file name to smthing identified :
+    // projectName_dateAdded.ext
+    const newName =
+      this.projectValue.projectFullName +
+      '_' +
+      new Date().toDateString() +
+      extension;
+    formData.append(
+      'uploadedImage',
+      this.fileUploadForm.get('uploadedImage').value,
+      newName
+    );
+    formData.append('agentId', '007');
 
-    this.http
-      .post('http://localhost:3033/project/getProjectMedia', formData)
-      .subscribe((response: any) => {
-        console.log('response received is ', response);
-      });
+    return this.http
+      .post<any>('http://localhost:3033/files/upload', formData)
+      .subscribe(
+        (response) => {
+          console.log(response);
+          if (response.statusCode === 200) {
+            // Reset the file input
+            this.uploadFileInput.nativeElement.value = '';
+            this.fileInputLabel = undefined;
+          }
+        },
+        (er) => {
+          console.log(er);
+          alert(er.error.error);
+        }
+      );
   }
 }
